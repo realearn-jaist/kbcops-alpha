04ebc898eb92ff630a504bb04eff2529df98db05
chaphowasit mahayossanan <chaphowasit.mah@suden.mahidol.edu>
2024-06-11 20:54:14 +0900

    - change embed-predict to only embed - create 4 embed model (optimizing)

--- backend/routes/routes.py
+++ backend/routes/routes.py
@@ -1,28 +1,18 @@
-import time
 from flask import jsonify, render_template, request
 from backend import app
-from ..controller import embedAndPredict
+from ..controller import embed
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/embed_predict', methods=['POST'])
-def embed_predict():
+
+@app.route("/embed", methods=["POST"])
+def embed_route():
     data = request.json
-    ontology = data['ontology']
-    algorithm = data['algorithm']
+    ontology = data["ontology"]
+    algorithm = data["algorithm"]
     print(f"Ontology: {ontology}, Algorithm: {algorithm}")
-    start_time = time.time()
-    result = embedAndPredict.embed_predict_func(
-        ontology_file="backend/ontologies/foodon-merged.train.owl",
-        embedding_dir="backend/cache/output",
-        config_file="backend/controller/default.cfg",
-        algorithm=algorithm
-    )
-    end_time = time.time()
-
-    execution_time = end_time - start_time
-    print(f"Execution time: {execution_time} seconds")
-    
+    result = embed.embed_func(ontology_file=ontology, algorithm=algorithm)
     return jsonify({"message": result})
